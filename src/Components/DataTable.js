ecad73fe6b25c01ef4dff0101a0fe81c0b3af0f9
vamshidevv <vamshi.bhandari.5057@gmail.com>
2024-08-13 12:49:25 +0530

    pass the table data through props, added profile image

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -7,51 +7,88 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import TablePagination from "@mui/material/TablePagination";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
-const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "Status", minWidth: 100 },
-  {
-    id: "population",
-    label: "Role",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Email address",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Teams",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
-  },
-];
+// const columns = [
+//   { id: "name", label: "Name", minWidth: 170 },
+//   { id: "code", label: "Status", minWidth: 100 },
+//   {
+//     id: "population",
+//     label: "Role",
+//     minWidth: 170,
+//     align: "right",
+//     format: (value) => value.toLocaleString("en-US"),
+//   },
+//   {
+//     id: "size",
+//     label: "Email address",
+//     minWidth: 170,
+//     align: "right",
+//     format: (value) => value.toLocaleString("en-US"),
+//   },
+//   {
+//     id: "density",
+//     label: "Teams",
+//     minWidth: 170,
+//     align: "right",
+//     format: (value) => value.toFixed(2),
+//   },
+// ];
 
-export default function ColumnGroupingTable() {
-  const [row, setRow] = useState([]);
+// function createData(name, code, population, size) {
+//   const density = population / size;
+//   return { name, code, population, size, density };
+// }
 
-  useEffect(() => {
-    const fetchMembers = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/members");
-        console.log("Json Response", response.data);
-        setRow(response.data);
-      } catch (error) {
-        console.error("Error fetching members:", error);
-      }
-    };
+// const rows = [
+//   createData("India", "IN", 1324171354, 3287263),
+//   createData("China", "CN", 1403500365, 9596961),
+//   createData("Italy", "IT", 60483973, 301340),
+//   createData("United States", "US", 327167434, 9833520),
+//   createData("Canada", "CA", 37602103, 9984670),
+//   createData("Australia", "AU", 25475400, 7692024),
+//   createData("Germany", "DE", 83019200, 357578),
+//   createData("Ireland", "IE", 4857000, 70273),
+//   createData("Mexico", "MX", 126577691, 1972550),
+//   createData("Japan", "JP", 126317000, 377973),
+//   createData("France", "FR", 67022000, 640679),
+//   createData("United Kingdom", "GB", 67545757, 242495),
+//   createData("Russia", "RU", 146793744, 17098246),
+//   createData("Nigeria", "NG", 200962417, 923768),
+//   createData("Brazil", "BR", 210147125, 8515767),
+// ];
 
-    fetchMembers();
-  }, []);
+export default function DataTable({ header, data }) {
+  // header and table data retreived with the the help of props
+  //   const [row, setRow] = useState({data});
+
+  //   useEffect(() => {
+  //     const fetchMembers = async () => {
+  //       try {
+  //         const response = await axios.get("http://localhost:3000/members");
+  //         console.log("Json Response", response.data);
+  //         setRow(response.data);
+  //       } catch (error) {
+  //         console.error("Error fetching members:", error);
+  //       }
+  //     };
+
+  //     fetchMembers();
+  //   }, []);
+
+  //   const [page, setPage] = React.useState(0);
+  //   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  //   const handleChangePage = (event, newPage) => {
+  //     setPage(newPage);
+  //   };
+
+  //   const handleChangeRowsPerPage = (event) => {
+  //     setRowsPerPage(+event.target.value);
+  //     setPage(0);
+  //   };
 
   return (
     <Paper sx={{ width: "100%" }}>
@@ -59,12 +96,20 @@ export default function ColumnGroupingTable() {
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              {columns.map((column) => (
+              {header.map((column) => (
                 <TableCell
                   id="tab-heading"
                   key={column.id}
                   align={column.align}
                   style={{ top: 0, minWidth: column.minWidth }}
+
+                  //   sx={{
+                  //     border: "1px solid green",
+                  //     textAlign: "left",
+                  //     color: "#667085",
+                  //     fontSize: "12px",
+                  //     fontWeight: 500,
+                  //   }}
                 >
                   {column.label}
                 </TableCell>
@@ -72,14 +117,20 @@ export default function ColumnGroupingTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {row.map((member) => (
+            {data.map((member) => (
               <TableRow
                 key={member.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                // sx={{ "&:last-child td, &:last-child th": { border: 0  , } }}
               >
-                <TableCell>
-                  <h4>{member.name}</h4>
-                  <p>{"@" + member.userName}</p>
+                <TableCell id="table-data">
+                  <div id="user-details-container">
+                    <input type="checkbox" />
+                    <img src={member.avatar} alt="testimg" id="profile-img" />
+                    <div id="user-name-container">
+                      <h4 id="name">{member.name}</h4>
+                      <p id="username">{"@" + member.userName}</p>
+                    </div>
+                  </div>
                 </TableCell>
                 <TableCell id="table-data">
                   <div
@@ -116,13 +167,26 @@ export default function ColumnGroupingTable() {
                 <TableCell id="table-data">{member.role}</TableCell>
                 <TableCell id="table-data">{member.email}</TableCell>
                 <TableCell id="table-data">
-                  {member.teams ? member.teams.join(", ") : ""}
+                  <div id="teams-container">
+                    <div id="teams-first">{member.teams[0]}</div>
+                    <div id="teams-second">{member.teams[1]}</div>
+                    <div id="teams-third">{member.teams[2]}</div>
+                  </div>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      {/* <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={row.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      /> */}
     </Paper>
   );
 }
--- src/Components/TeamMembers.jsx
+++ src/Components/TeamMembers.jsx
@@ -4,6 +4,30 @@ import axios from "axios";
 
 const TeamMembers = () => {
   const [data, setData] = useState([]);
+  const [header, setHeader] = useState([]);
+
+  const columns = [
+    { id: "name", label: "Name", minWidth: 170 },
+    { id: "status", label: "Status", minWidth: 100 },
+    {
+      id: "role",
+      label: "Role",
+      minWidth: 170,
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "email",
+      label: "Email address",
+      minWidth: 170,
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "teams",
+      label: "Teams",
+      minWidth: 170,
+      format: (value) => value.toFixed(2),
+    },
+  ];
 
   useEffect(() => {
     const fetchMembers = async () => {
@@ -15,8 +39,9 @@ const TeamMembers = () => {
         console.error("Error fetching members:", error);
       }
     };
-
+    setHeader(columns);
     fetchMembers();
+
   }, []);
 
   console.log("Team members Data : ", data);
@@ -31,11 +56,19 @@ const TeamMembers = () => {
           </div>
 
           <div className="header-rigth-section">
-            <button className="btn-delete-all">Delete Selected</button>
+            <button
+              className="btn-delete-all "
+              onClick={() => {
+                console.log("delete all btn is clicked");
+              }}
+            >
+              Delete Selected
+            </button>
           </div>
         </div>
         <div className="table-container">
-          <DataTable data={data} />
+          {/*passed the header and table data with the help of props*/}
+          <DataTable header={header} data={data} />
         </div>
       </div>
     </div>
